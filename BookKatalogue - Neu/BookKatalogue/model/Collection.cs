99cbd2eb3b1fffbe6c7c6afd437f54e8d6c55ab1
Christopher-Lenz <naraxxi@googlemail.com>
2015-12-24 13:57:28 +0100

    Collection implementieren (Teil 1)

---
+++ BookKatalogue - Neu/BookKatalogue/model/Collection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookKatalogue.model
+{
+    class Collection
+    {
+        private List<CollectionItem> _collectionList = new List<CollectionItem>();
+
+        public Collection()
+        {
+            
+        }
+
+        public void AddCollection(CollectionItem item)
+        {
+            _collectionList.Add(item);
+        }
+
+        public void RemoveCollection(CollectionItem item)
+        {
+            _collectionList.Remove(item);
+        }
+    }
+}
